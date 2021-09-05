5244f5420f0f5277d37e90e55752a447b60162d4
Justice Dreischor <t-justice98@hotmail.com>
2021-09-05 19:14:20 +0200

    initial commit

---
+++ Week1/PA1.0/src/main/java/MyFirstClass.java
@@ -0,0 +1,23 @@
+public class MyFirstClass {
+    int age;
+    String name;
+    double weight;
+
+    public MyFirstClass(int age, String name, double weight) {
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
---
+++ Week1/PA1.0/src/test/java/MyFirstClassTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyFirstClassTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getWeight() {
+    }
+}
